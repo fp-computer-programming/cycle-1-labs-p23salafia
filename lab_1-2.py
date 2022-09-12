66c9e0994c32c9a91ddaee7fb5cd4365eda62b62
Jan <p23jsalafia@fairfieldprep.org>
2022-09-12 09:26:05 -0400

    Submit Cycle 1 Day 6 Labs

---
+++ lab_1-2.py
@@ -0,0 +1,6 @@
+# Author: JMS 09/12/2022
+first_name = 'Jan Salafia'
+print(first_name)
+
+    favorite_hobby = 'Play Video Games'
+    print(favorite_hobby)                       #Improper Indentation
